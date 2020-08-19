11a53cbcd757a34546aad7f50781711a7cfe94f8
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 20:15:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine === 0){
+    return "There is nobody waiting to be served!"
+  } else {return `currently serving ${katzDeliLine.shift()}.`
+}
+}
