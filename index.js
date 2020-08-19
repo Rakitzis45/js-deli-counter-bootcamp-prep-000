a8beaa75eefa18bb5f78371a1a9f5f2447bca351
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 20:01:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, newName){
   for (i = 0; i < katzDeliLine.length; i++ )
   katzDeliLine.push(newName[i]);
-} return katzDeliLine
+return katzDeliLine
+}
