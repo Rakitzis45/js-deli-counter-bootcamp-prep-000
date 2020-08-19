0e9e945fc35e08d2c7684633cff6591dce3689c4
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 20:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine){
   let line = [];
+  for (let i = 0; i <katzDeliLine.length; i++){
+    line.push(``+ [i + 1] + `.` + katzDeliLine[i]);
+  }
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
-  } else if (katzDeliLine.length > 0){ i = 0; i < katzDeliLine.length; i++
-  } return `The line is currently ${line.length}`
+  }
 }
